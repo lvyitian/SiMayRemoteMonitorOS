3df5ef6cd9ee39f91241fb83aab97a582718db3c
koko <596461462@qq.com>
2019-11-12 18:24:04 +0800

    服务(未完成)

--- SiMay.RemoteClient.NewCore/Service.cs
+++ SiMay.RemoteClient.NewCore/Service.cs
@@ -1,4 +1,7 @@
-﻿using SiMay.ServiceCore.Win32;
+﻿using SiMay.Core;
+using SiMay.ServiceCore.Win32;
+using SiMay.Sockets.Tcp;
+using SiMay.Sockets.Tcp.TcpConfiguration;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -6,6 +9,7 @@ using System.Data;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Reflection;
 using System.ServiceProcess;
 using System.Text;
@@ -15,6 +19,11 @@ namespace SiMay.ServiceCore
 {
     public partial class Service : ServiceBase
     {
+        struct UserProcessToken
+        {
+            public int SessionId { get; set; }
+
+        }
         public Service()
         {
             InitializeComponent();
@@ -26,6 +35,51 @@ namespace SiMay.ServiceCore
             //用户进程启动
             var result = Win32Interop.OpenInteractiveProcess(openProcessString + " \"-user\"", desktopName, true, out _);
 
+            var serverConfig = new TcpSocketSaeaServerConfiguration();
+            serverConfig.AppKeepAlive = true;
+            serverConfig.PendingConnectionBacklog = 0;
+            var trunkService = TcpSocketsFactory.CreateServerAgent(TcpSocketSaeaSessionType.Packet, serverConfig,
+                (notity, session) =>
+            {
+                switch (notity)
+                {
+                    case TcpSocketCompletionNotify.OnConnected:
+                        break;
+                    case TcpSocketCompletionNotify.OnSend:
+                        break;
+                    case TcpSocketCompletionNotify.OnDataReceiveing:
+                        break;
+                    case TcpSocketCompletionNotify.OnDataReceived:
+                        break;
+                    case TcpSocketCompletionNotify.OnClosed:
+                        break;
+                    default:
+                        break;
+                }
+            });
+
+            bool completed = false;
+            for (int trycount = 0; trycount < 100; trycount++)
+            {
+                try
+                {
+                    var ipEndPoint = new IPEndPoint(IPAddress.Any, 10000 + trycount);
+                    trunkService.Listen(ipEndPoint);
+                    completed = true;
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    LogHelper.WriteErrorByCurrentMethod("trunkService open listen exception:" + ex.Message);
+                }
+                completed = false;
+                Thread.Sleep(1000);
+            }
+
+            LogHelper.WriteErrorByCurrentMethod("listen all tcp port not completed，please check!");
+
+            if (!completed)
+                Environment.Exit(0);//监听所有端口失败
         }
 
         protected override void OnStop()
